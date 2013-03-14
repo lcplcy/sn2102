e1022ddd156671bca939f637b8d1437e0ace7edb
lcplcy <lcp_lcy@hotmail.com>
2013-03-14 14:13:18 +0800

    Updated index.php with db connection details.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 # This function reads your DATABASE_URL configuration automatically set by Heroku
 # the return value is a string that will work with pg_connect
 function pg_connection_string() {
-  return "dbname=dcmnp0r7gqo428 host=ec2-54-243-39-42.compute-1.amazonaws.com port=5432 user=xcltjptyzxhxne password=XkjS3U0aTZhMXmOdRb82B8GGDe sslmode=require"
+  return "dbname=dcmnp0r7gqo428 host=ec2-54-243-39-42.compute-1.amazonaws.com port=5432 user=xcltjptyzxhxne password=XkjS3U0aTZhMXmOdRb82B8GGDe sslmode=require";
 }
  
 # Establish db connection
